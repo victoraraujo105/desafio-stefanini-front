988eb61affa3a2a98437f4d0a2d20cb2d8a682b8
Victor Araújo <victor.araujo105@gmail.com>
2024-04-01 17:33:16 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,24 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'desafio-stefanini-front';
+  users: Usuario[] = [];
+
+  ngOnInit() {
+    this.getAllUsers().then(
+      response => this.users = response
+    );
+  }
+
+  constructor() {
+    this.getAllUsers().then(
+      response => this.users = response
+    );
+  }
+
+  async getAllUsers(): Promise<Usuario[]> {
+    const data = await fetch('http://localhost:8080/usuarios');
+    let ret = await data.json() ?? [];
+    console.log(ret);
+    return ret;
+  }
 }
